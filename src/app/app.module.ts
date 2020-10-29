a504e625fc216d26bdc7f7a2a7c63e0b17f8c50d
leidy lopez <leidy.lopez@imexhs.com>
2020-10-29 07:04:38 -0500

    Se crea modulo para usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GraphComponent } from './graph/graph.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraphComponent
+    GraphComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
